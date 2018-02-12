84c2ae80adee46a860e1078d055a43f8a1979c22
Tina <fiskhe@outlook.com>
2018-02-12 03:07:32 -0800

    heckin hek yah

--- nn_ada.py
+++ nn_ada.py
@@ -6,9 +6,11 @@ dataframe = pd.read_csv('irises_processed.csv')
 data = dataframe['Data']
 iris_type = dataframe['Class']
 
-learn_rate = 0.01
+ada_learn_rate = 0.01
+rms_learn_rate = 0.001
 epsilon = 1e-8
-num_epochs = 1000
+gamma = 0.9
+num_epochs = 100000
 sum_grad0 = 0
 sum_grad1 = 0
 
@@ -26,13 +28,35 @@ def sigmoid(x, deriv = False):
     else:
         return 1/(1 + np.exp(x))
 
-def adagrad(lr, sum_g, epsilon):
-    return np.divide(lr, np.sqrt(sum_g + epsilon))
+def adagrad(lr, sum_g, grad, eps):
+    sum_g += np.square(grad)
+    new_lr = np.divide(lr, np.sqrt(sum_g + eps))
+    return [sum_g, new_lr]
 
-for epoch in range(num_epochs):
+def rmsprop(lr, sum_g, grad, eps, gam):
+    sum_g = gam * sum_g + (1 - gam) * np.square(grad) 
+    new_lr = np.divide(lr, np.sqrt(sum_g + eps))
+    return [sum_g, new_lr]
+
+def eval_acc(real, pred):
+    accurate = False
+    # The endes of the greatest value int he actual vector
+    r_index = np.where(real == real.max())[1]
+    # The index of the greatest value in predicted vector
+    p_index = np.where(pred == pred.max())[1]
+        
+    if r_index == p_index:
+        accurate = True
+            
+    return accurate
     
-    total_err = np.array([[0.,0.,0.]])
+first = True
+
+for epoch in range(num_epochs): 
+    total_train_acc = 0
+    total_test_acc = 0
     
+    # Training
     for ind in range(120):
         layer0 = data[ind].split('|')
         layer0 = [float(x) for x in layer0]
@@ -47,8 +71,12 @@ for epoch in range(num_epochs):
 
         lay2_err = true_output - layer2
         lay1_err = np.dot(lay2_err, weights1.T)
-        
-        total_err += lay2_err
+
+        ######## Gathering data ########
+        ######## A.K.A. Metrics ########
+        if eval_acc(true_output, layer2):
+            total_train_acc += 1
+        ################################
         
         # Use of the backpropagation algorithm
         w1_delt = lay2_err * sigmoid(layer2, True)
@@ -57,14 +85,51 @@ for epoch in range(num_epochs):
         w1_grad = np.dot(layer1.T, w1_delt)
         w0_grad = np.dot(layer0.T, w0_delt)
         
-        sum_grad0 += np.square(w0_grad)
-        sum_grad1 += np.square(w1_grad)
+        # Using Adagrad
+        # changes0 = adagrad(ada_learn_rate, sum_grad0, w0_grad, epsilon)
+        # changes1 = adagrad(ada_learn_rate, sum_grad1, w1_grad, epsilon)
+        
+        # Using rmsprop
+        if first:
+            changes0 = rmsprop(rms_learn_rate, sum_grad0, w0_grad, epsilon, 0)
+            changes1 = rmsprop(rms_learn_rate, sum_grad1, w1_grad, epsilon, 0)
+        else:
+            changes0 = rmsprop(rms_learn_rate, sum_grad0, w0_grad, epsilon, gamma)
+            changes1 = rmsprop(rms_learn_rate, sum_grad1, w1_grad, epsilon, gamma)
 
-        weights0 -= adagrad(learn_rate, sum_grad0, epsilon) * w0_grad
-        weights1 -= adagrad(learn_rate, sum_grad1, epsilon) * w1_grad
+        sum_grad0 = changes0[0]
+        sum_grad1 = changes1[0]
 
-    # Print out total error over epoch every 100 epochs
-    if epoch % 100 == 0:
-        print(total_err)
+        new_lr0 = changes0[1]
+        new_lr1 = changes1[1]
+
+        weights0 -= new_lr0 * w0_grad
+        weights1 -= new_lr1 * w1_grad
+
+    # Testing
+    for ind in range(30):
+        i = 120 + ind
+        layer0 = data[i].split('|')
+        layer0 = [float(x) for x in layer0]
+        layer0 = np.array([layer0])
+        true_output = iris_type[i].split('|')
+        true_output = [float(x) for x in true_output]
+        true_output = np.array([true_output])
+
+        layer1 = sigmoid(np.dot(layer0, weights0)) # 1 x 4 matrix
+        layer2 = sigmoid(np.dot(layer1, weights1)) # 1 x 3 matrix
+
+        if eval_acc(true_output, layer2):
+            total_test_acc += 1
+
+        
+    # Print out total error over epoch every 1000 epochs
+    if epoch % 10000 == 0:
+        print('total training accurate')
+        print(total_train_acc)
+        print(total_train_acc/120)
+        print('total testing accurate')
+        print(total_test_acc)
+        print(total_test_acc/30)
         
         
